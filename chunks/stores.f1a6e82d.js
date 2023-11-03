c8ff431aeff45721da189fcb9aa7417a1a01d7ce
tsu-nam-i <149790959+tsu-nam-i@users.noreply.github.com>
2023-11-04 11:42:50 +1300

    Create stores.f1a6e82d.js

---
+++ chunks/stores.f1a6e82d.js
@@ -0,0 +1,19 @@
+import {s as e} from "./singletons.2572eeef.js";
+const r = ()=>{
+    const s = e;
+    return {
+        page: {
+            subscribe: s.page.subscribe
+        },
+        navigating: {
+            subscribe: s.navigating.subscribe
+        },
+        updated: s.updated
+    }
+}
+  , b = {
+    subscribe(s) {
+        return r().page.subscribe(s)
+    }
+};
+export {b as p};
